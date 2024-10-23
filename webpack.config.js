b49bedf91585a2a2bbb29cac8b51ce4f052252a7
shimo24 <salar.sani24@gmail.com>
2024-10-23 10:27:11 +0200

    done

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require("path");
+
+module.exports = {
+    entry: "./app/index.js",
+    module:{
+        rulse:[
+            {
+                test: /\.svg$/,
+                use: 'svg-inline-loader'
+            },
+
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            }, 
+
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }}
+            }
+        ]
+    },
+    output:{
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+
+    Plugins: [new HtmlWebpackPlugin()],
+    mode:"production"
+}
